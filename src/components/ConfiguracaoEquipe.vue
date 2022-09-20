46acae0c5ccfa8d953e81e96a4354b865f8d72b7
Fernando Andrade <fernandow.and@gmail.com>
2022-09-20 13:15:44 -0300

    337. mapState - Mapeando o estado da Store como propriedades computadas parte 1

--- src/components/ConfiguracaoEquipe.vue
+++ src/components/ConfiguracaoEquipe.vue
@@ -7,31 +7,36 @@
     </div>
     <div class="row">
       <div class="col-8">
-        <p>Enfermeiro:</p>
-        <p>Socorrista:</p>
-        <p>Médico:</p>
-        <p>Carro:</p>
-        <p>Telefone:</p>
-        <p>Kit de reanimação:</p>
+        <p>Enfermeiro: {{ equipe.enfermeiro }}</p>
+        <p>Socorrista: {{ equipe.socorrista }}</p>
+        <p>Médico: {{ equipe.medico }}</p>
+        <p>Carro: {{ equipe.carro }}</p>
+        <p>Telefone: {{ equipe.telefone }}</p>
+        <p>Kit de reanimação: {{ equipe.kitDeReanimacao }}</p>
       </div>
       <div class="col-4 text-center">
         <div class="row">
           <div class="col">
-            <img class="img-fluid" :src="require('@/assets/ambulancias/indefinida.png')">
+            <img
+              class="img-fluid"
+              :src="require('@/assets/ambulancias/indefinida.png')"
+            />
           </div>
         </div>
         <div class="row mt-3">
           <div class="col">
             <button type="button" class="btn btn-primary">Montar equipe</button>
           </div>
-        </div>              
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
-    name: 'ConfiguracaoEquipe'
-}
+  name: "ConfiguracaoEquipe",
+  computed: mapState(["equipe"]),
+};
 </script>
--- src/store.js
+++ src/store.js
@@ -2,6 +2,14 @@ import Vuex from "vuex";
 
 export default new Vuex.Store({
   state: {
-    titulo: "## Emergências Médicas ##"
+    titulo: "## Emergências Médicas ##",
+    equipe: {
+      enfermeiro: "Nome do enfermeiro",
+      socorrista: "Nome do socorrista",
+      medico: "Nome do médico",
+      carro: "Placa do carro",
+      telefone: "+55 11 99545-5411",
+      kitDeReanimacao: "Kit 0001",
+    },
   },
 });
