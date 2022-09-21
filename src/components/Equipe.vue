16d795a566d3a1b1076b65905c4aea9eb057ec35
Fernando Andrade <fernandow.and@gmail.com>
2022-09-21 11:35:22 -0300

    365. Listando equipes montadas parte 1

---
+++ src/components/Equipe.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Listagem Equipe</div>
+</template>
+
+<script>
+export default {
+  name: "Equipe",
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/ListaEquipes.vue
@@ -0,0 +1,14 @@
+<template>
+  <div><equipe /></div>
+</template>
+
+<script>
+import Equipe from "./Equipe.vue";
+export default {
+  name: "ListaEquipes",
+  components: { Equipe },
+};
+</script>
+
+<style>
+</style>
