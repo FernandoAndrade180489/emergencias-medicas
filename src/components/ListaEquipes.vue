f87e3c31b4d03770279625cdcd61a12a36f0a4b9
Fernando Andrade <fernandow.and@gmail.com>
2022-09-21 11:42:26 -0300

    366. Listando equipes montadas parte 2

--- src/components/ListaEquipes.vue
+++ src/components/ListaEquipes.vue
@@ -1,12 +1,26 @@
 <template>
-  <div><equipe /></div>
+  <div>
+    <ul class="list-group">
+      <li
+        class="list-group-item"
+        v-for="(equipe, indice) in equipes"
+        :key="indice"
+      >
+        <equipe :dados="equipe" />
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
 import Equipe from "./Equipe.vue";
 export default {
   name: "ListaEquipes",
   components: { Equipe },
+  computed: {
+    ...mapState(["equipes"]),
+  },
 };
 </script>
 
